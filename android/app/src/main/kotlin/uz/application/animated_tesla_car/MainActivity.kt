19696236acb8f66d1892d598260381bfe861c364
ilhomjon <catchy977@gmail.com>
2022-09-07 21:03:48 +0500

    Starting-Project

---
+++ android/app/src/main/kotlin/uz/application/animated_tesla_car/MainActivity.kt
@@ -0,0 +1,6 @@
+package uz.application.animated_tesla_car
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:animated_tesla_car/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tesla Animated App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
